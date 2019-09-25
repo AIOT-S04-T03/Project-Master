395f2a5f5f0487101ae183f811817a3648db90a0
user <user@example.com>
2019-09-25 14:29:17 +0800

    Create db sqls and java bean model

---
+++ Database_JavaBean/db_sql.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `project_t03_platedetection`.`car_management` ( `id` INT(3) NOT NULL AUTO_INCREMENT , `car_plate` VARCHAR(255) NOT NULL , `enter_time` VARCHAR(255) NOT NULL , `car_place` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+CREATE TABLE `project_t03_platedetection`. ( `id` INT NOT NULL AUTO_INCREMENT , `car_plate` VARCHAR(255) NOT NULL , `isEmpty` BOOLEAN NOT NULL , `place_name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`place_name`)) ENGINE = InnoDB;
