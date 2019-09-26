a7166a1717143e65faaec88fcb4f923c1dd84d6d
user <user@example.com>
2019-09-26 19:47:31 +0800

    SQL modified

--- Database_JavaBean/db_sql.sql
+++ Database_JavaBean/db_sql.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE `project_t03_platedection`;
+CREATE DATABASE `project_t03_platedetection`;
 
 CREATE TABLE `project_t03_platedetection`.`car_management` ( `id` INT(3) NOT NULL AUTO_INCREMENT , `car_plate` VARCHAR(255) NOT NULL , `enter_time` VARCHAR(255) NOT NULL , `car_place` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
