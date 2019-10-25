db1a4a8d414482d681e62a412068d9caf4075d40
user <user@example.com>
2019-10-25 15:27:28 +0800

    Data control modules update

---
+++ test/src/utils/service/server/Server.java
@@ -0,0 +1,122 @@
+package utils.service.server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+
+import com.sun.swing.internal.plaf.metal.resources.metal;
+
+import jdk.nashorn.internal.parser.JSONParser;
+import jdk.nashorn.internal.runtime.regexp.joni.Config;
+import utils.service.datacontrol.DataControl;
+
+
+// Server module with thread implement
+public class Server implements Runnable {
+
+	private int port = 0;
+	private Socket socket = null;
+	
+	private BufferedReader reader = null;
+	private PrintWriter writer = null;
+	private DataControl dataControl = null;
+	
+	private Object prevSendData = null;
+	
+	public Server(Socket socket, int port, DataControl dataControl) throws Exception {
+		
+		super();
+		this.socket = socket;
+		this.port = port;
+		this.dataControl = dataControl;
+		
+		InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+		reader = new BufferedReader(isReader);
+		writer = new PrintWriter(socket.getOutputStream());
+	}
+	
+	public void destroy() throws Exception {
+		
+		socket.close();
+		reader.close();
+		writer.close();
+		socket = null;
+		reader = null;
+		writer = null;
+	}
+	
+	// Thread start
+	public void run() {
+
+		try {
+
+			dataControl.setPort(port);
+			dataControl.setWriter(writer);
+			String data = "";
+			
+			while(port == 5001) {
+				
+				if(dataControl.getData().getPlaceData().isEmpty())	
+					continue;
+				
+				HashMap<String, String> sendData = dataControl.getData().getPlaceData();
+				
+				if(sendData.equals(prevSendData))	
+					continue;
+
+				dataControl.sendData(sendData);
+				prevSendData = sendData;
+				dataControl.getData().setPlaceData(new HashMap<String, String>());
+			}
+			
+			while((data = reader.readLine()) != null) {
+				
+				if(port == 5000) {
+					
+					System.out.println("Port " + port + " recv: " + data);
+					dataControl.recvData(5000, data);
+					data = "";
+				}
+			
+				
+				if(port == 5002) {
+					
+					System.out.println("Port " + port + " recv: " + data);
+					dataControl.recvData(5002, data);
+					data = "";
+				}
+				
+				if(port == 5005) {
+					
+					System.out.println("Port " + port + " recv: " + data);
+					dataControl.recvData(5005, data);
+					data = "";
+				}
+				/*
+				System.out.println("Port " + port + " recv: " + data);
+				data = data.replace("\\", "");
+				data = data.substring(1, data.length() - 1); 
+				System.out.println("JSON format: " + data);
+				dataControl.recvData(data);
+				writer.println(data);
+				writer.println("1.jpg=RCJ9785");
+				writer.flush();
+				data = "";
+				*/
+			}
+			
+			System.out.println("Port " + port + " disconnect...");
+		}
+		
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	// End Thread
+}
---
+++ test/src/utils/service/server/ServerMain.java
@@ -0,0 +1,69 @@
+package utils.service.server;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import utils.service.datacontrol.DataControl;
+
+public class ServerMain {
+	
+	public void startServer(DataControl[] dataControls, int nPorts) throws Exception {
+
+		Thread[] threads = new Thread[nPorts];
+			
+		for(int i = 0 ; i < nPorts ; i++) {
+			
+			threads[i] = new Thread(new PortListeningThread(5000 + i, dataControls[i]));
+			threads[i].start();
+		}
+		
+		for(int i = 0 ; i < nPorts ; i++) {
+			
+			threads[i].join();
+		}
+	}
+	
+	public class PortListeningThread implements Runnable {
+		
+		private int port = 0;
+		private ServerSocket serverSocket = null;
+		private DataControl dataControl = null;
+		
+		public PortListeningThread(int port, DataControl dataControl) {
+
+			super();
+			this.port = port;
+			this.dataControl = dataControl;
+		}
+		
+		public void run() {
+			
+			try {
+				
+				serverSocket = new ServerSocket(port);
+				System.out.println("Port " + port + " listening... ");
+			} 
+			
+			catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			while(true) {
+				
+				try {
+
+					Socket socket = serverSocket.accept();
+					Thread t = new Thread(new Server(socket, port, dataControl));
+					t.start();
+					System.out.println("Port " + port + " got a connection");
+				} 
+				
+				catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
